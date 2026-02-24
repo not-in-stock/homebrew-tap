799b82da1bd6d627449f055625edd95fc7c7a3e7
Andrey Fedorov <oran9e.red@gmail.com>
2026-02-24 09:27:03 +0100

    Add Cask и kanata-bar workflow

---
+++ Casks/kanata-bar.rb
@@ -0,0 +1,11 @@
+cask "kanata-bar" do
+  version "0.1.0"
+  sha256 "TODO"
+
+  url "https://github.com/not-in-stock/kanata-bar/releases/download/v#{version}/kanata-bar.app.zip"
+  name "Kanata Bar"
+  desc "macOS menu bar app for kanata keyboard remapper"
+  homepage "https://github.com/not-in-stock/kanata-bar"
+
+  app "Kanata Bar.app"
+end
